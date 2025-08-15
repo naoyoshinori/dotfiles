52dab85318fd22167d394756abb40e3cb576b376
Naoyuki Yoshinori <1448309+naoyoshinori@users.noreply.github.com>
2025-08-15 11:06:07 +0000

    feat: Enhance setup.bat with robust symlink creation and existing file handling

--- setup.bat
+++ setup.bat
@@ -1,3 +1,25 @@
+@ECHO OFF
+
 CD /D "%~dp0"
 
-mklink "%USERPROFILE%\.gitconfig" "%CD%\.gitconfig"
+IF EXIST "%USERPROFILE%\.gitconfig" (
+    CHOICE /C YN /M "The file .gitconfig already exists. Do you want to delete it?"
+    IF ERRORLEVEL 2 (
+        ECHO Aborting setup.
+        EXIT /B
+    ) ELSE (
+        DEL "%USERPROFILE%\.gitconfig"
+    )
+)
+MKLINK "%USERPROFILE%\.gitconfig" "%CD%\config\git\.gitconfig"
+
+IF EXIST "%USERPROFILE%\.gemini" (
+    CHOICE /C YN /M "The directory .gemini already exists. Do you want to delete it?"
+    IF ERRORLEVEL 2 (
+        ECHO Aborting setup.
+        EXIT /B
+    ) ELSE (
+        RMDIR /S /Q "%USERPROFILE%\.gemini"
+    )
+)
+MKLINK /D "%USERPROFILE%\.gemini" "%CD%\config\gemini"
